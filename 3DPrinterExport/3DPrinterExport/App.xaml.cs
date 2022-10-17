299abb6e624294e476dc34a78b51cbe2158807aa
esimiele <simiele77@gmail.com>
2022-10-17 10:40:30 -0400

    Converted plugin to executable

---
+++ 3DPrinterExport/3DPrinterExport/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace _3DPrinterExport
+{
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Window mw;
+            mw = new _3DPrinterExport.MainWindow(e);
+            mw.Show();
+        }
+    }
+}
---
+++ 3DPrinterExport/3DPrinterExport/Launcher/launch3DPrinterExport.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(ScriptContext context)
+        {
+            try
+            {
+                if (context.Patient != null)
+                {
+                    string exeName = "3DPrinterExport";
+                    string path = AppExePath(exeName);
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        ProcessStartInfo p = new ProcessStartInfo(path);
+                        if (context.Patient != null) p.Arguments = String.Format("{0} {1}", context.Patient.Id, context.StructureSet.Id);
+                        Process.Start(p);
+                    }
+                    else MessageBox.Show(String.Format("Error! {0} executable NOT found!", exeName));
+                }
+                else MessageBox.Show("Please open a patient before launching the autoplanning tool!");
+            }
+            catch (Exception e) { MessageBox.Show(e.Message); };
+        }
+        private string AppExePath(string exeName)
+        {
+            return FirstExePathIn(Path.GetDirectoryName(GetSourceFilePath()), exeName);
+        }
+
+        private string FirstExePathIn(string dir, string exeName)
+        {
+            return Directory.GetFiles(dir, "*.exe").FirstOrDefault(x => x.Contains(exeName));
+        }
+
+        private string GetSourceFilePath([CallerFilePath] string sourceFilePath = "")
+        {
+            return sourceFilePath;
+        }
+    }
+}
